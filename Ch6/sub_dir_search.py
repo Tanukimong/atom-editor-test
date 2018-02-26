4ac1f70265bb129d276b4638dbadd8c137ad0683
Hyeonho Shin <tlsgusghq@hanyang.ac.kr>
2018-02-26 17:55:15 +0900

    180226 공부 내용

---
+++ Ch6/sub_dir_search.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import os
+
+def search(dirname = "/Users/Tanukimong/github" ):
+    try:
+        filenames = os.listdir(dirname)
+        for filename in filenames:
+            full_filename = os.path.join(dirname,filename)
+            if os.path.isdir(full_filename):
+                search(full_filename)
+            else:
+                ext = os.path.splitext(full_filename)[-1]
+                if ext == '.py':
+                    print(full_filename)
+    except PermissionError:
+        pass
+
+search("/Users/Tanukimong/github")
---
+++ Ch6/tabto4.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+#python tabto4.py a.txt b.txt
+
+import sys
+
+src = sys.argv[1]
+dst = sys.argv[2]
+
+f = open(src)
+tab_content = f.read()
+f.close()
+
+space_content = tab_content.replace("\t"," "*4)
+
+f = open(dst,"w")
+f.write(space_content)
+f.close()
