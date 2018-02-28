b86c3e2043737d8a5daa4d0ad18ad2eaf89cfb1e
Hyeonho Shin <tlsgusghq@hanyang.ac.kr>
2018-02-28 20:34:21 +0900

    Drone Seminar에서 when_start()함수를 두 번이나 만들었는데 각각이 독립된 스레드처럼 작동

---
+++ droneSeminar/중복선언체크.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# 엔트리봇 오브젝트의 파이선 코드
+
+import time
+
+def when_start():
+    while True:
+        time.sleep(1)
+        print("Hey!")
+
+def when_start():
+    while True:
+        time.sleep(2)
+        print("Nyah-")
+
+when_start()
